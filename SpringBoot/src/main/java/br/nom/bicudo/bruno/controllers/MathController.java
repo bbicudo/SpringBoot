dafd5c6fdd1db3d7736ad6a8e8ed01234ba8b597
Bruno Branco Bicudo <bruno.bicudo@connectbr.dev>
2024-12-26 00:40:52 -0300

    Change folder structure

--- SpringBoot/src/main/java/br/nom/bicudo/bruno/controllers/MathController.java
+++ SpringBoot/src/main/java/br/nom/bicudo/bruno/controllers/MathController.java
@@ -1,4 +1,4 @@
-package br.nom.bicudo.bruno;
+package br.nom.bicudo.bruno.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;

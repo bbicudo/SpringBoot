6616d6c0832e9500ca0331d0c4615ee6dcc693b4
Bruno Branco Bicudo <bruno.bicudo@connectbr.dev>
2024-12-25 23:38:51 -0300

    Implements math endpoint.

---
+++ 05_FirstSteps/SpringBoot/src/main/java/br/nom/bicudo/bruno/MathController.java
@@ -0,0 +1,55 @@
+package br.nom.bicudo.bruno;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.nom.bicudo.bruno.exceptions.UnsupportedMathOperationException;
+
+@RestController
+public class MathController {
+	
+	private static final String template = "Hello, %s!";
+	private final AtomicLong counter = new AtomicLong();
+	
+	@RequestMapping(value = "/sum/{numberOne}/{numberTwo}", method=RequestMethod.GET)
+	public Double sum(
+		@PathVariable(value = "numberOne") String numberOne,
+		@PathVariable(value = "numberTwo") String numberTwo
+	) throws Exception {
+		if (
+			!isNumeric(numberOne) ||
+			!isNumeric(numberTwo)
+		) {
+			throw new UnsupportedMathOperationException("Please set a numeric value!");
+		}
+		return convertToDouble(numberOne) + convertToDouble(numberTwo);
+	}
+
+	private Double convertToDouble(String strNumber) {
+		if (strNumber == null) { 
+			return 0D;
+		}
+		String number = strNumber.replaceAll(",", ".");
+
+		if (isNumeric(number)) {
+			return Double.parseDouble(number);
+		}
+		
+		return 0D;
+	}
+
+	private boolean isNumeric(String strNumber) {
+		if (strNumber == null) { 
+			return false;
+		}
+		
+		String number = strNumber.replaceAll(",", ".");
+		
+		return number.matches("[-+]?[0-9]*\\.?[0-9]+");
+	}
+}

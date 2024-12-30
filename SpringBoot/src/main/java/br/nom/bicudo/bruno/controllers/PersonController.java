e5639a9ae9d4d2cd988a24f69c5d24bbd144e0fe
Bruno Branco Bicudo <bruno.bicudo@connectbr.dev>
2024-12-30 16:33:20 -0300

    Prepare API for versioning.

--- SpringBoot/src/main/java/br/nom/bicudo/bruno/controllers/PersonController.java
+++ SpringBoot/src/main/java/br/nom/bicudo/bruno/controllers/PersonController.java
@@ -19,7 +19,7 @@ import br.nom.bicudo.bruno.data.vo.v1.PersonVO;
 import br.nom.bicudo.bruno.services.PersonServices;
 
 @RestController
-@RequestMapping("/person")
+@RequestMapping("/api/person/v1")
 public class PersonController {
 	
 	@Autowired

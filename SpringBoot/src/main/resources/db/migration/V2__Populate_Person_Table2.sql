50260821e13b765502db3c32c6f26d8886131c06
Bruno Branco Bicudo <bruno.bicudo@connectbr.dev>
2024-12-30 17:41:27 -0300

    Implements FlyWay dependencies and create migration scripts for Person table in database.

---
+++ SpringBoot/src/main/resources/db/migration/V2__Populate_Person_Table2.sql
@@ -0,0 +1,3 @@
+LOCK TABLES `person` WRITE;
+INSERT INTO `person` VALUES (4,'Bragança Paulista - Cerejeiras 3 - BR','Bruno Branco','Male','Bicudo'),(5,'Bragança Paulista - Cerejeiras 3 - BR','Bianca Rodrigues','Female','Silva'),(6,'Bragança Paulista - Cerejeiras 3 - BR','Tchuchu','Female','Gatinha');
+UNLOCK TABLES;

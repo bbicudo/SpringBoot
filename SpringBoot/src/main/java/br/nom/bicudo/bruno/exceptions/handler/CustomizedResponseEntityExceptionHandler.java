0ab41435273416ed85004498930db3d3ebe19dad
Bruno Branco Bicudo <brunobranco4@hotmail.com>
2024-12-30 15:32:01 -0300

    Feature/database integration (#2)
    * Implementation of POST/PUT/DELETE methods and mappings, with the correct annotations.
    
    * Implements database integration.
    
    * Fix indentation.
    
    ---------
    
    Co-authored-by: Bruno Branco Bicudo <bruno.bicudo@connectbr.dev>

--- SpringBoot/src/main/java/br/nom/bicudo/bruno/exceptions/handler/CustomizedResponseEntityExceptionHandler.java
+++ SpringBoot/src/main/java/br/nom/bicudo/bruno/exceptions/handler/CustomizedResponseEntityExceptionHandler.java
@@ -11,14 +11,17 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import br.nom.bicudo.bruno.exceptions.ExceptionResponse;
-import br.nom.bicudo.bruno.exceptions.UnsupportedMathOperationException;
+import br.nom.bicudo.bruno.exceptions.ResourceNotFoundException;
 
 @ControllerAdvice
 @RestController
 public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler{
 
 	@ExceptionHandler(Exception.class)
-	public final ResponseEntity<ExceptionResponse> handleAllExceptions(Exception ex, WebRequest request) {
+	public final ResponseEntity<ExceptionResponse> handleAllExceptions(
+		Exception ex,
+		WebRequest request
+	) {
 		ExceptionResponse exceptionResponse = new ExceptionResponse(
 			new Date(),
 			ex.getMessage(),
@@ -26,5 +29,19 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
 		);
 		
 		return new ResponseEntity<>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public final ResponseEntity<ExceptionResponse> handleNotFoundException(
+		Exception ex,
+		WebRequest request
+	) {
+		ExceptionResponse exceptionResponse = new ExceptionResponse(
+			new Date(),
+			ex.getMessage(),
+			request.getDescription(false)
+		);
+		
+		return new ResponseEntity<>(exceptionResponse, HttpStatus.NOT_FOUND);
 	}
 }
--- SpringBoot/src/main/java/br/nom/bicudo/bruno/services/PersonServices.java
+++ SpringBoot/src/main/java/br/nom/bicudo/bruno/services/PersonServices.java
@@ -1,70 +1,73 @@
 package br.nom.bicudo.bruno.services;
 
-import java.util.ArrayList;
+
 import java.util.List;
-import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.nom.bicudo.bruno.exceptions.ResourceNotFoundException;
 import br.nom.bicudo.bruno.model.Person;
+import br.nom.bicudo.bruno.repositories.PersonRepository;
 
 @Service
 public class PersonServices {
 
-	private final AtomicLong counter = new AtomicLong();
+
 	private Logger logger = Logger.getLogger(PersonServices.class.getName());
 	
+	@Autowired
+	PersonRepository repository;
+	
 	public List<Person> findAll() {
-		List<Person> persons = new ArrayList<>();
 		logger.info("Finding all people.");
-		for (int i = 0; i < 8; i++) {
-			Person person = mockPerson(i);
-			persons.add(person);
-		}
 
-		return persons ;
+		return repository.findAll() ;
 	}
 
 	public Person create(Person person) {
 		logger.info("Creating a person");
 
-		return person;
-	}
 
-	public Person update(Person person) {
-		logger.info("Updating a person");
-		
-		return person;
+		return repository.save(person);
 	}
-
-	public void delete(String id) {
-		logger.info("Deleting a person");
-	}
-
-	public Person findById(String id) {
+	
+	public Person findById(Long id) {
 		logger.info("Finding a person.");
 		
 		Person person = new Person();
-		person.setId(counter.incrementAndGet());
 		person.setFirstName("Bruno");
 		person.setLastName("Bicudo");
 		person.setAddress("Bragança Paulista - SP - BR");
 		person.setGender("Male");
 		
-		return person;
-	}
-	
 
-	private Person mockPerson(int i) {
-		Person person = new Person();
-		person.setId(counter.incrementAndGet());
-		person.setFirstName("Bruno" + i);
-		person.setLastName("Bicudo" + i);
-		person.setAddress("Bragança Paulista - SP - BR" + i);
-		person.setGender("Male");
+		return repository.findById(id)
+			.orElseThrow(() -> new ResourceNotFoundException("No records found for this id."));
+	}
+
+	public Person update(Person person) {
+		logger.info("Updating a person");
 		
-		return person;
+		Person entity = repository.findById(person.getId())
+			.orElseThrow(() -> new ResourceNotFoundException("No records found for this id."));
+		
+		entity.setFirstName(person.getFirstName());
+		entity.setLastName(person.getLastName());
+		entity.setAddress(person.getAddress());
+		entity.setGender(person.getGender());
+
+		return repository.save(person);
+	}
+
+	public void delete(Long id) {
+		logger.info("Deleting a person");
+		
+		Person entity = repository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("No records found for this id."));
+		
+		repository.delete(entity);
 	}
 	
 }

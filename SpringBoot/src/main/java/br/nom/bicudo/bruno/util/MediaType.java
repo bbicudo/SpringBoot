ffb900772828e58db557a83982f09caf83f898e2
Bruno Branco Bicudo <brunobranco4@hotmail.com>
2025-01-05 15:26:56 -0300

    Add support to HATEOAS. (#6)
    Co-authored-by: Bruno Branco Bicudo <bruno.bicudo@connectbr.dev>

--- SpringBoot/src/main/java/br/nom/bicudo/bruno/util/MediaType.java
+++ SpringBoot/src/main/java/br/nom/bicudo/bruno/util/MediaType.java
@@ -2,7 +2,7 @@ package br.nom.bicudo.bruno.util;
 
 public class MediaType {
 
-	public static final String APPLICATION_YAML = "application/x-yaml";
 	public static final String APPLICATION_JSON = "application/json";
+	public static final String APPLICATION_YAML = "application/x-yaml";
 	public static final String APPLICATION_XML = "application/xml";
 }
